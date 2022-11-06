5dac1e4a7fcd13705a0d2a6b57ba069a15631acd
Ibrahim Dogrusoz <ibrahim.dogrusoz@ae.be>
2022-11-06 20:23:28 +0100

    Implement the logic for calculating floors

---
+++ src/main/java/day01/CalculateFloors.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class CalculateFloors {
+
+    private final String POSITIVE_VALUE = "\\(";
+    public int calculateFloors(String str) {
+        var negativeString = str.replaceAll(POSITIVE_VALUE, "");
+        return str.length() - negativeString.length() * 2;
+    }
+}
---
+++ src/test/java/day01/CalculateFloorsTest.java
@@ -0,0 +1,24 @@
+package day01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculateFloorsTest {
+    CalculateFloors calculator = new CalculateFloors();
+
+    @Test
+    void givenEqualAmountOfParenthesis_whenCalculateFloor_itCalculatesCorrectly() {
+        var str = "(())";
+        var actual = calculator.calculateFloors(str);
+        assertEquals(actual, 0);
+    }
+
+    @Test
+    void givenNegativeValueOfParenthesis_whenCalculateFloor_itCalculatesCorrectly() {
+        var str = ")())())";
+        var actual = calculator.calculateFloors(str);
+        assertEquals(actual, -3);
+    }
+}
